608c8551d759de2faa8bbd59f3b5e5af5604ff7a
zisov4eto <zisov4eto@gmail.com>
2016-06-03 00:58:10 +0300

    Added demos and exercises for Algorithms course

---
+++ ALGORITHMS/Demos/Recursion-Demos/Generating-Combinations/CombinationsGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class CombinationsGenerator
+{
+    static void Main()
+    {
+        int n = 3;
+        int startNum = 0;
+        int endNum = 5;
+
+        int[] arr = new int[n];
+        GenCombs(arr, 0, startNum, endNum);
+    }
+
+    static void GenCombs(int[] arr, int index, int startNum, int endNum)
+    {
+        if (index >= arr.Length)
+        {
+            // A combination was found --> print it
+            Console.WriteLine("(" + string.Join(", ", arr) + ")");
+        }
+        else
+        {
+            for (int i = startNum; i <= endNum; i++)
+            {
+                arr[index] = i;
+                GenCombs(arr, index + 1, i + 1, endNum);
+            }
+        }
+    }
+}
---
+++ ALGORITHMS/Exercises/RecursionAndRecursiveAlgorithms/01.RecursiveArraySum/RecursiveArraySum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    class RecursiveArraySum
+    {
+        static void Main()
+        {
+            Console.WriteLine("Type several integer numbers separated by single space:");
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int sum = FindSum(array, 0);
+        }
+
+        private static int FindSum(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                return 0;
+            }
+
+            return array[index] + FindSum(array, index + 1);
+        }
+    }
+}
